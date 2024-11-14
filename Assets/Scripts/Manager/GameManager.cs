53b4aa4c3b129135236cfee9aafd0670ca6a3753
Huidong Shin <dongdongcompany@khu.ac.kr>
2024-11-14 09:24:50 +0900

    UPDATE : Main Intro Video

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    //GameManager singleton instance
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        //singleton pattern implementation
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+        DontDestroyOnLoad(gameObject);
+    }
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
