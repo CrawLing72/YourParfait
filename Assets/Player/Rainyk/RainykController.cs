d12d24e079e9ddf5b7ad690f3259eaae8b2e133b
CrawLing72 <dongdongcompay@khu.ac.kr>
2024-11-21 14:12:00 +0900

    Rainyk Controller (temp)

--- Assets/Player/Rainyk/RainykController.cs
+++ Assets/Player/Rainyk/RainykController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 
-public class SeraphinaController : BasicController, IAttack
+public class RainykController : BasicController, IAttack
 {
     protected NetworkObject destroyObj;
     protected override void Start()
@@ -11,16 +11,13 @@ public class SeraphinaController : BasicController, IAttack
         base.Start();
 
     }
-    protected override void InputActionW() 
+    protected override void InputActionW() // Current: Seraphina Controller CTRC, CTRV. WARNING!
     {
         if (isWAble)
         {
             if (Input.GetKeyDown(KeyCode.W))
             {
-                float currentHp = stat.GetCurrentHp();
-                float healAmount = 150.0f;
-                stat.SetCurrentHp(currentHp + healAmount);
-                GetAdBuff(50.0f, 3);
+                // Buff Effect Not Implemented
                 Vector3 interpolation = new Vector3(0f, 2.5f, 0);
                 NetworkObject obj = NetworkManager.Instance.runner.Spawn(skillWPreFeb, transform.position - interpolation, Quaternion.identity);
                 obj.gameObject.transform.SetParent(gameObject.transform, true);
@@ -32,7 +29,7 @@ public class SeraphinaController : BasicController, IAttack
             }
         }
     }
-    protected override void InputActionE() 
+    protected override void InputActionE()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
@@ -118,7 +115,7 @@ public class SeraphinaController : BasicController, IAttack
                 skill.SetSkillDamage(10.0f); // need Change
                 attack.transform.rotation = Quaternion.AngleAxis(angel, new Vector3(0, 0, 1));
                 attack.transform.position = myPos + dir * 2.0f;
-                
+
 
 
                 isQAble = false;
